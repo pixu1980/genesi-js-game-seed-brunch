054a4ee34452ba9a30212755bbdf6b7feeceff75
bwbuser <bwbuser2@iMacBlack.local>
2017-06-23 11:38:19 +0200

    - temp commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,8 @@
-import Game from './Game';
+// import Game from './Game';
 import config from '../config';
 import locales from '../locales';
 
 document.addEventListener('DOMContentLoaded', () => {
-  window.onload = new Game(config.environment, config.manifests, locales);
+  debugger;
+  // window.onload = new Game(config.environment, config.manifests, locales);
 });
